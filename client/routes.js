e2620f3d02117624fc001aa1eb9192301a6e49a3
Soheil Hamidi <soheilhamidi@me.com>
2018-05-04 10:14:01 -0400

    first commit

---
+++ client/routes.js
@@ -0,0 +1,29 @@
+import { Router } from 'meteor/iron:router';
+import { OHIF } from 'meteor/ohif:core';
+
+Router.configure({
+    layoutTemplate: 'layout',
+    loadingTemplate: 'layout'
+});
+
+Router.onBeforeAction('loading');
+
+Router.route('/', function() {
+    Router.go('studylist', {}, { replaceState: true });
+}, { name: 'home' });
+
+Router.route('/studylist', function() {
+    this.render('ohifViewer', { data: { template: 'studylist' } });
+}, { name: 'studylist' });
+
+Router.route('/viewer/:studyInstanceUids', function() {
+    const studyInstanceUids = this.params.studyInstanceUids.split(';');
+    OHIF.viewerbase.renderViewer(this, { studyInstanceUids }, 'ohifViewer');
+}, { name: 'viewerStudies' });
+
+// OHIF #98 Show specific series of study
+Router.route('/study/:studyInstanceUid/series/:seriesInstanceUids', function () {
+    const studyInstanceUid = this.params.studyInstanceUid;
+    const seriesInstanceUids = this.params.seriesInstanceUids.split(';');
+    OHIF.viewerbase.renderViewer(this, { studyInstanceUids: [studyInstanceUid], seriesInstanceUids }, 'ohifViewer');
+}, { name: 'viewerSeries' });
