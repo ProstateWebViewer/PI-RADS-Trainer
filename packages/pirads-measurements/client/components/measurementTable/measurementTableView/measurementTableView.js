a125b0acd7e9fd72b4443dd627e7d76b1faf13b0
Soheil Hamidi <soheilhamidi@me.com>
2018-07-06 12:06:12 -0400

    Minor fixes, orthanc dynamic user pass and changes in reading in CSV

--- packages/pirads-measurements/client/components/measurementTable/measurementTableView/measurementTableView.js
+++ packages/pirads-measurements/client/components/measurementTable/measurementTableView/measurementTableView.js
@@ -51,161 +51,127 @@ function descriptionMap(seriesDescription) {
 }
 
 async function displayFiducials(instance) {
-  // OHIF.viewerbase.toolManager.setActiveTool('probe');
-  // const currentTool = OHIF.viewerbase.toolManager.getActiveTool();
-  $('#probe').trigger("click");
-  await wait(1);
-
-  const studyInstanceUid = OHIF.viewerbase.layoutManager.viewportData[Session.get('activeViewport')]['studyInstanceUid'];
   const patientName = instance.data.studies[0].patientName;
   const fiducials = Fiducials.find({ ProxID: patientName }).fetch();
-  const delay = 2000;
-  // const element = $('.imageViewerViewport')[Session.get('activeViewport')];
-  // const image = cornerstone.getEnabledElement(element).image;
-  // const imagePlane = cornerstone.metaData.get('imagePlaneModule', image.imageId);
-  // console.log(imagePlane);
-  // const sliceThickness = cornerstone.metaData.get('instance', image.imageId)['sliceThickness'];
-  $('.imageViewerViewport').each((ind, ele) => {
-      const imageId = cornerstone.getEnabledElement(ele).image.imageId;
-      const seriesDescription = cornerstone.metaData.get('series', imageId)['seriesDescription'];
-      fiducials.forEach(async (val, index) => {
-          const imagePoint = val[descriptionMap(seriesDescription)];
-          // const flag = true
-          // const imagaIndex = (flag) ? (cornerstone.metaData.get('series', imageId).numImages - imagePoint.z) : (imagePoint.z);
-          // console.log(cornerstone.metaData.get('series', imageId).numImages);
-          const imagaIndex = cornerstone.metaData.get('series', imageId).numImages - imagePoint.z - 1;
-          function scroll() {
-            return new Promise(resolve => {
-              setTimeout(() => {
-                cornerstoneTools.scrollToIndex(ele, imagaIndex);
-                resolve('resolved');
-              }, delay * (index + 1));
-            });
-          }
-
-          const measurementData = {
-            'f_id': val.fid,
-            'ClinSig': val.ClinSig,
-            'server': true,
-            'visible': true,
-            'active': true,
-            'color': (val.ClinSig) ? '#ee6002' : '#90ee02',
-            'invalidated': true,
-            'handles': {
-              'end': {
-                'active': true,
-                'highlight': true,
-                'x': imagePoint.x,
-                'y': imagePoint.y
-              }
-            }
-          };
-          await scroll();
-          await wait((delay/2) * (index + 1));
-          $(ele).off('cornerstonetoolsmeasurementadded');
-          cornerstoneTools.addToolState(ele, 'probe', measurementData);
-          cornerstone.updateImage(ele);
-          bindToMeasurementAdded(ele);
-      });
-  });
-
-  // await wait(1);
-  // $('#probe').trigger("click");
-
-  // console.log(cornerstone.metaData.get('instance', image.imageId));
-  // console.log(imagePlane.imagePositionPatient.z);
-
-  // fiducials.forEach(async (val, index) => {
-    // console.log(val.pos.z);
-    // const imagaIndex = Math.floor(Math.abs(imagePlane.imagePositionPatient.z - val.pos.z)/sliceThickness) - 2;
-    // const seriesDescription = cornerstone.metaData.get('series', image.imageId);
-
-
-
-    // const delay = 1000;
-    //
-    // function scroll() {
-    //   return new Promise(resolve => {
-    //     setTimeout(() => {
-    //       cornerstoneTools.scrollToIndex(element, imagaIndex);
-    //       resolve('resolved');
-    //     }, delay * (index + 1));
-    //   });
-    // }
-    //
-    // await scroll();
-    // await wait((delay/2) * (index + 1));
-
-    // console.log(imagaIndex);
-    // const patientPoint = new cornerstoneMath.Vector3(val.pos.x, val.pos.y, val.pos.z);
-    //
-    // const imagePoint = cornerstoneTools.projectPatientPointToImagePlane(patientPoint, imagePlane);
-    // const measurementData = {
-    //   'f_id': val.fid,
-    //   'ClinSig': val.ClinSig,
-    //   'server': true,
-    //   'visible': true,
-    //   'active': true,
-    //   'color': (val.ClinSig) ? '#ee6002' : '#90ee02',
-    //   'invalidated': true,
-    //   'handles': {
-    //     'end': {
-    //       'active': true,
-    //       'highlight': true,
-    //       'x': imagePoint.x,
-    //       'y': imagePoint.y
-    //     }
-    //   }
-    // };
-    // cornerstoneTools.addToolState(element, 'probe', measurementData);
-  // });
-
-  function findingsAnalysis() {
-
-    let str = '';
-
-    fiducials.forEach((val) => {
-      const minDistance = Number.MAX_SAFE_INTEGER;
-      const f_id = 0;
-      const patientPoint = new cornerstoneMath.Vector3(val.pos.x, val.pos.y, val.pos.z);
-      fiducialsCollection.find({'studyInstanceUid': studyInstanceUid}).fetch().forEach((value) => {
-          const distance = patientPoint.distanceTo(value.patientPoint).toFixed(2)
-          if (distance < minDistance) {
-            minDistance = distance;
-            f_id = value.id;
-          }
-      });
-      if (f_id) {
-        str = str.concat(
-          'fid '+ f_id + ' is closest to ',
-          (val.ClinSig) ? 'CSPC-' + val.fid : 'CIPC-' + val.fid,
-          ' with ' + minDistance + ' mm\n'
-        );
-      }
-    });
-
-    return str;
-  }
-
   const ClinSigCounter = fiducials.filter(v => v.ClinSig).length;
 
-  instance.feedbackString.set(''.concat(
-    'An expert radiologist indicated ',
-    fiducials.length.toString(),
-    (fiducials.length === 1) ? ' area ' : ' areas ',
-    'of suspicion for this patient, and the patient underwent MR-guidance biopsies.\n\n',
-    'Biopsy results:\n',
-    ClinSigCounter,
-    ' clinical significant',
-    (ClinSigCounter === 1) ? ' finding ' : ' findings ',
-    '(Gleason score 7 or higher) ',
-    (ClinSigCounter === 1) ? 'was ' : 'were ',
-    'identified in the pathology report.\n\n',
-    'Analysis of your findings:\n',
-    findingsAnalysis(),
-  ));
-  // alert(fiducials.length.toString() + ' locations were biopsied.');
-  $('#wwwc').trigger("click");
+  if (!('pos' in fiducials[0])) {
+      instance.feedbackString.set(''.concat(
+        'Annotations:\n',
+        'Expert radiologist indicated ',
+        fiducials.length.toString(),
+        (fiducials.length === 1) ? ' suspicious area ' : ' suspicious areas ',
+        'for this patient.',
+        '\n\nProcedure:\n',
+        'The patient underwent MR-guidance biopsies.\n\n',
+        'Biopsy results:\n',
+        ClinSigCounter,
+        ' clinical significant',
+        (ClinSigCounter === 1) ? ' finding ' : ' findings ',
+        '(Gleason score 7 or higher) ',
+        (ClinSigCounter === 1) ? 'was ' : 'were ',
+        'reported by pathology.\n\n'
+      ));
+      $('#wwwc').trigger("click");
+  }
+  else {
+      $('#probe').trigger("click");
+      await wait(1);
+
+
+      const studyInstanceUid = OHIF.viewerbase.layoutManager.viewportData[Session.get('activeViewport')]['studyInstanceUid'];
+      const delay = 2000;
+
+      $('.imageViewerViewport').each((ind, ele) => {
+          const imageId = cornerstone.getEnabledElement(ele).image.imageId;
+          const seriesDescription = cornerstone.metaData.get('series', imageId)['seriesDescription'];
+          fiducials.forEach(async (val, index) => {
+              const imagePoint = val[descriptionMap(seriesDescription)];
+              const imagaIndex = cornerstone.metaData.get('series', imageId).numImages - imagePoint.z - 1;
+              function scroll() {
+                return new Promise(resolve => {
+                  setTimeout(() => {
+                    cornerstoneTools.scrollToIndex(ele, imagaIndex);
+                    resolve('resolved');
+                  }, delay * (index + 1));
+                });
+              }
+
+              const measurementData = {
+                'f_id': val.fid,
+                'ClinSig': val.ClinSig,
+                'server': true,
+                'visible': true,
+                'active': true,
+                'color': (val.ClinSig) ? '#ee6002' : '#90ee02',
+                'invalidated': true,
+                'handles': {
+                  'end': {
+                    'active': true,
+                    'highlight': true,
+                    'x': imagePoint.x,
+                    'y': imagePoint.y
+                  }
+                }
+              };
+              await scroll();
+              await wait((delay/2) * (index + 1));
+              $(ele).off('cornerstonetoolsmeasurementadded');
+              cornerstoneTools.addToolState(ele, 'probe', measurementData);
+              cornerstone.updateImage(ele);
+              bindToMeasurementAdded(ele);
+          });
+      });
+
+      function findingsAnalysis() {
+
+        let str = '';
+
+        fiducials.forEach((val) => {
+          const minDistance = Number.MAX_SAFE_INTEGER;
+          const f_id = 0;
+          const patientPoint = new cornerstoneMath.Vector3(val.pos.x, val.pos.y, val.pos.z);
+          fiducialsCollection.find({'studyInstanceUid': studyInstanceUid}).fetch().forEach((value) => {
+              const distance = patientPoint.distanceTo(value.patientPoint).toFixed(2)
+              if (distance < minDistance) {
+                minDistance = distance;
+                f_id = value.id;
+              }
+          });
+          if (f_id) {
+            str = str.concat(
+              'fid '+ f_id + ' is the closest to ',
+              (val.ClinSig) ? 'CSPC-' + val.fid : 'CIPC-' + val.fid,
+              ' with ' + minDistance + ' mm\n'
+            );
+          }
+        });
+
+        return str;
+      }
+
+
+
+      instance.feedbackString.set(''.concat(
+        'Annotations:\n',
+        'Expert radiologist indicated ',
+        fiducials.length.toString(),
+        (fiducials.length === 1) ? ' suspicious area ' : ' suspicious areas ',
+        'for this patient.',
+        '\n\nProcedure:\n',
+        'The patient underwent MR-guidance biopsies.\n\n',
+        'Biopsy results:\n',
+        ClinSigCounter,
+        ' clinical significant',
+        (ClinSigCounter === 1) ? ' finding ' : ' findings ',
+        '(Gleason score 7 or higher) ',
+        (ClinSigCounter === 1) ? 'was ' : 'were ',
+        'reported by pathology.\n\n',
+        'Analysis of your findings:\n',
+        findingsAnalysis(),
+      ));
+      $('#wwwc').trigger("click");
+  }
 }
 
 Template.measurementTableView.onCreated(() => {
