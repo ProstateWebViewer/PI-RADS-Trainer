33df5ccbda9191345b385f19c6a05aaec6b7457a
Soheil Hamidi <soheilhamidi@me.com>
2018-07-09 13:04:30 -0400

    Disable dicom server modal and orthanc volumes added

--- client/components/ohifViewer/ohifViewer.js
+++ client/components/ohifViewer/ohifViewer.js
@@ -9,11 +9,6 @@ Template.ohifViewer.onCreated(() => {
     instance.headerClasses = new ReactiveVar('');
 
     OHIF.header.dropdown.setItems([{
-        action: () => OHIF.ui.showDialog('serverInformationModal'),
-        text: 'Server Information',
-        icon: 'fa fa-server fa-lg',
-        separatorAfter: true
-    }, {
         action: () => OHIF.ui.showDialog('userPreferencesDialog'),
         text: 'Preferences',
         icon: 'fa fa-user',
